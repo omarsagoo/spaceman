7cafadab310a164dbb27f1ea6e77dd5ee17bcd7e
omarsagoo <om.sagoo@gmail.com>
2019-09-10 22:45:47 -0700

    fixed UI issues/ added comments

--- spaceman.py
+++ spaceman.py
@@ -12,13 +12,13 @@ def load_word():
 
 #Handles all user input with error handling
 #Jake Shams showed me this recursive solution
-def user_input(prompt):
-    usr_input = input(prompt)
-    if len(usr_input) != 1:
-        return user_input("Input can only be one letter: ")
-    if not usr_input.isalpha():
-        return user_input("Input must be a letter: ")
-    return usr_input
+def input_handler(prompt):
+    user_input = input(prompt)
+    if len(user_input) != 1:
+        return input_handler("Input can only be one letter: ")
+    if not user_input.isalpha():
+        return input_handler("Input must be a letter: ")
+    return user_input.lower()
 
 # creates an array to store the Users correct guesses
 def word_init(secret_word):
@@ -34,7 +34,7 @@ def find_index(secret_word, guess):
     
     return indicies
 
-#
+# adds the letter to the guessed array
 def submit_letter(char, underscore, indicies):
     for index in indicies:
         underscore[index] = char
@@ -43,11 +43,14 @@ def submit_letter(char, underscore, indicies):
 
 def main(secret_word):
     word_guess = word_init(secret_word)
-    lives = 7
-    #guessed_ltrs = list()
+    lives = len(secret_word)
+    guessed_ltrs = list()
     while lives > 0 and secret_word != ''.join(word_guess):
-        guess = user_input("Guess a letter! ")
-        #guessed_ltrs.append(guess)
+        print(' '.join(word_guess))
+        guess = input_handler("Guess a letter! ")
+        while guess in guessed_ltrs:
+            guess = input_handler("Already guessed that letter. try a different one: ")
+        guessed_ltrs.append(guess)
         find_index(secret_word, guess)
         indicies = find_index(secret_word, guess)
         if len(indicies) > 0:
@@ -59,7 +62,7 @@ def main(secret_word):
             print('Sorry thats a bad guess!')
             lives -= 1
             print('You have ' + str(lives) + ' guesses left')
-       # print(guessed_ltrs)
+        print('You have guessed these letters: ' + ', '.join(guessed_ltrs))
     if lives == 0:
         print('Sorry better luck next time!')
     else:
@@ -70,13 +73,13 @@ def main(secret_word):
 def test():
     print(secret_word)
 
-    user_input("hello! ")
+    input_handler("hello! ")
 
     word_init(secret_word)
 
 
 
 secret_word = load_word()
-
-# print('Welcome to the spaceman game! ')
-# main(secret_word)
+name = input("What is your name? ")
+print('Welcome to the spaceman game, ' + name + '!')
+main(secret_word)
